4d63eab317013fc4104d651d115ecc5c4bbeb136
gregwinther <sebastian.winther-larsen@menon.no>
2021-12-01 08:32:17 +0100

    Solve day 1 part 2

--- 2021/day_01.py
+++ 2021/day_01.py
@@ -1,3 +1,5 @@
+# Part 1
+
 with open("./input_01.txt") as f:
     input_strings = f.readlines()
 
@@ -12,3 +14,17 @@ for i in range(1, n):
         increased[i] = 1
 
 print(f"{sum(increased)} measurements are larger than the previous.")
+
+# Part 2: Three measurement sliding window
+
+three_measurements_sum = []
+for i in range(3, n+1):
+    three_measurements_sum.append(sum(data[i-3:i]))
+
+m = len(three_measurements_sum)
+increased_three = [0] * m
+for i in range(1, m):
+    if three_measurements_sum[i - 1] < three_measurements_sum[i]:
+        increased_three[i] = 1
+
+print(f"Number of increases in windows of 3: {sum(increased_three)}")
