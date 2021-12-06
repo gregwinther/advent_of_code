854efb0f171df1e48b0f71ac9d4b24eeac6bfb8e
gregwinther <sebastian.winther-larsen@menon.no>
2021-12-06 18:02:31 +0100

    Solve day 4 part 1

---
+++ 2021/day_05.py
@@ -0,0 +1,37 @@
+import numpy as np
+# from matplotlib import pyplot as plt
+
+# Part 1
+
+with open("./input_05.txt", "r") as f:
+    data = f.readlines()
+
+data = list(map(lambda string: list(map(int, string.replace(" -> ", ",").split(","))), data))
+data = np.array(data)
+
+grid = np.zeros([data.max()] * 2)
+
+for coords in data:
+    x1, y1, x2, y2 = coords
+
+    # Only vertical
+    if x1 == x2:
+        if y1 < y2:
+            grid[x1, y1:y2+1] += 1
+        elif y1 > y2:
+            grid[x1, y2:y1+1] += 1
+        else:
+            grid[x1, y1] += 1
+
+    # Only horizontal
+    if y1 == y2:
+        if x1 < x2:
+            grid[x1:x2+1, y1] += 1
+        elif x1 > x2:
+            grid[x2:x1+1, y1] += 1
+
+# plt.imshow(grid)
+# plt.show()
+
+intersections = (grid > 1).sum()
+print("No. of intersections:", intersections)
