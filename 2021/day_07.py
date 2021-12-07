90c10be960fc58df349c9162ca5ea5c322b4029f
gregwinther <sebastian.winther-larsen@menon.no>
2021-12-07 09:15:52 +0100

    Solve day 7 part 1

--- 2021/day_07.py
+++ 2021/day_07.py
@@ -1,7 +1,6 @@
 import numpy as np 
 
-# Test case
-positions = np.array([16,1,2,0,4,2,7,1,2,14], dtype=int)
-
+# positions = np.array([16,1,2,0,4,2,7,1,2,14], dtype=int)
+positions = np.loadtxt("./input_07.txt", dtype=int, delimiter=",")
 cost = np.abs(positions - np.median(positions)).sum()
-print(cost)
+print(f"Part 1 sol'n: {cost}")
