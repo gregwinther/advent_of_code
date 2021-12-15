5a8d1257bef6814b7abaa3706b516916c0c8fbe7
gregwinther <sebastian.winther-larsen@menon.no>
2021-12-15 22:37:41 +0100

    Solve day 9 part 1
        Finally! Initial error: Included diagonal neighbours in binary structure.

---
+++ 2021/day_09.py
@@ -0,0 +1,47 @@
+import numpy as np
+from scipy.ndimage.filters import minimum_filter
+from scipy.ndimage.morphology import binary_erosion, generate_binary_structure
+from matplotlib import pyplot as plt
+
+with open("./input_09.txt", "r") as f:
+    data = f.readlines()
+
+data = list(map(lambda string: list(string)[:-1], data))
+data = np.array(data, dtype=int)
+
+## Test data
+# data = np.array(
+#     [
+#         [2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
+#         [3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
+#         [9, 8, 5, 6, 7, 8, 9, 8, 9, 2],
+#         [8, 7, 6, 7, 8, 9, 6, 7, 8, 9],
+#         [9, 8, 9, 9, 9, 6, 5, 6, 7, 8],
+#     ]
+# )
+
+## IVAN's detect peaks function
+def detect_local_minima(data):
+    neighborhood = generate_binary_structure(len(data.shape), 1)
+    local_min = (minimum_filter(data, footprint=neighborhood)==data)
+    background = (data==9)
+    eroded_background = binary_erosion(
+        background, structure=neighborhood, border_value=1
+    )
+    detected_minima = local_min ^ eroded_background
+    return np.where(detected_minima), detected_minima
+
+minima_coords, minima = detect_local_minima(data)
+# print("Minima coordinates:\n", minima_coords)
+print("Minima values:\n", data[minima_coords])
+risk_levels = data[minima_coords] + 1
+print("Risk levels:\n", risk_levels, "\nSum:", risk_levels.sum())
+for i, j in zip(*minima_coords):
+    value = data[i, j] 
+    if value == 9:
+        print(i, j, data[i, j])
+plt.figure()
+plt.imshow(data)
+plt.figure()
+plt.imshow(minima)
+plt.show()
