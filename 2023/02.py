e0861888c6bcd4199855ee442fe34cd2e6876efa
Sebastian Gregorius Winther-Larsen <s.winther.larsen@gmail.com>
2023-12-02 14:33:30 +0000

    Cont. day 02. Keyboad not working.

--- 2023/02.py
+++ 2023/02.py
@@ -8,11 +8,12 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
 
+lines = test_data.split("\n")
+
 # What games would be possible if the bag contained 
 # only 12 red cubes, 13 green cubes, and 14 blue cubes?
 
-game_dic = {}
+game_dict = {}
 for line in lines:
-    game_id = re.search(r"(?<=Game )(\d+)(?=:)")
-    print(game_id)
-    # game_dict[game_id]
+    game_id = re.search(r"(?<=Game )(\d+)(?=:)", line).group(1)
+    game_dict[int(game_id)] = line.split(":")[1]
