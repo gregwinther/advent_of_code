b7e70585a670505fb0b1864553fb6097309e68ba
gregwinther <s.winther.larsen@gmail.com>
2023-12-06 14:08:03 +0100

    Day 6 part two done :)

--- 2023/06.py
+++ 2023/06.py
@@ -30,3 +30,35 @@ for time, dist in zip(race_times, distances):
 ways_to_beat_race = [len(bht) for bht in button_hold_times]
 
 print("Product of ways to beat race:", prod(ways_to_beat_race))
+
+## Part II
+
+# Test data
+race_time = 71530
+distance = 940200
+
+# Actual data
+race_time = 35937366
+distance = 212206012011044
+
+def compute_distance(hold_time, race_time):
+    speed = hold_time
+    time_left = race_time - hold_time
+    distance_travelled = speed * time_left
+    return distance_travelled
+
+# lowest time to hold button
+for hold_time in range(race_time):
+    distance_travelled = compute_distance(hold_time, race_time) 
+    if distance_travelled > distance:
+        low_hold_time = hold_time
+        break
+
+# longest time to hold button
+for hold_time in range(race_time, 0, -1):
+    distance_travelled = compute_distance(hold_time, race_time)
+    if distance_travelled > distance:
+        high_hold_time = hold_time
+        break
+
+print("Ways to beat the record: ", high_hold_time - low_hold_time + 1)
