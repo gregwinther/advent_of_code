3834267cd2dde2e0cf568ec0235c95f7892252a3
Sebastian Gregorius Winther-Larsen <s.winther.larsen@gmail.com>
2023-12-10 21:49:50 +0000

    Day 8 part two.
    LCM baby! :)

--- 2023/08.py
+++ 2023/08.py
@@ -7,6 +7,17 @@ from itertools import cycle
 # BBB = (AAA, ZZZ)
 # ZZZ = (ZZZ, ZZZ)"""
 
+# input_data = """LR
+# 
+# 11A = (11B, XXX)
+# 11B = (XXX, 11Z)
+# 11Z = (11B, XXX)
+# 22A = (22B, XXX)
+# 22B = (22C, 22C)
+# 22C = (22Z, 22Z)
+# 22Z = (22B, 22B)
+# XXX = (XXX, XXX)"""
+
 with open("./input08.txt") as f:
     input_data = f.read().strip()
 
@@ -14,15 +25,20 @@ directions, raw = input_data.split("\n\n")
 
 nodes = {}
 for line in raw.split("\n"):
-    key, left, right = re.findall(r"\w{3}", line)
+    key, left, right = re.findall(r"[\w\d]{3}", line)
     nodes[key] = {"L": left, "R": right}
 
-directions = cycle(directions) # Amazing trick!!
-
-def solve(position = "AAA"): 
+def solve(position = "AAA", directions=cycle("LR")): 
     for i, dir in enumerate(directions):
         if position[-1] == "Z":
             return i
         position = nodes[position][dir]
 
-print("Steps: ", solve("AAA"))
+## print("Steps: ", solve("AAA")) 
+
+starting_points = [node for node in nodes if node[-1] == "A"]
+solns = [solve(node, cycle(directions)) for node in starting_points]
+
+from math import lcm
+
+print("Least common multiplier: ", lcm(*solns))
