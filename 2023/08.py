8eb5ff5486574d457d3af149d68530a3fd0a8630
Sebastian Gregorius Winther-Larsen <s.winther.larsen@gmail.com>
2023-12-10 21:28:38 +0000

    Day 8 part one - check.

---
+++ 2023/08.py
@@ -0,0 +1,27 @@
+import re
+from itertools import cycle
+
+# input_data = """LLR
+# 
+# AAA = (BBB, BBB)
+# BBB = (AAA, ZZZ)
+# ZZZ = (ZZZ, ZZZ)"""
+
+with open("./input08.txt") as f:
+    input_data = f.read().strip()
+
+directions, raw = input_data.split("\n\n")
+
+nodes = {}
+for line in raw.split("\n"):
+    key, left, right = re.findall(r"\w{3}", line)
+    nodes[key] = {"L": left, "R": right}
+
+directions = cycle(directions) # Amazing trick!!
+position = "AAA"
+for i, dir in enumerate(directions):
+    if position == "ZZZ":
+        break
+    position = nodes[position][dir]
+
+print("Steps: ", i)
