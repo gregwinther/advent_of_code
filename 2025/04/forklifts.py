4b16d437409126ba0a4abb92608c61253815afe6
gregwinther <s.winther.larsen@gmail.com>
2025-12-04 13:25:20 +0100

    Solve day 4 part 2

--- 2025/04/forklifts.py
+++ 2025/04/forklifts.py
@@ -8,11 +8,9 @@ with open(input_path, "r") as file:
     lines = [list(line.strip()) for line in lines]
     paper_map = np.array(lines)
 
-m, n = paper_map.shape
-
 # Check N, NE, E, SE, S, SW, W, NW for "@" and count steps to "."
 
-def check_direction(start_i, start_j, delta_i, delta_j):
+def check_direction(start_i, start_j, delta_i, delta_j, m, n):
     i, j = start_i + delta_i, start_j + delta_j
     if (i < 0 or i >= m or j < 0 or j >= n):
         return 0
@@ -23,25 +21,39 @@ def check_direction(start_i, start_j, delta_i, delta_j):
 
 # print(paper_map)
 
-good_positions = []
+def find_good_positions(paper_map):
 
-for i in range(m):
-    for j in range(n):
-        if paper_map[i, j] == ".":
-            continue
-        # Check each direction. Check how far I can move (not outside map)
-        # If there are fewer than four "@" in all directions combined, save pos (good_positions)
-        total_at_count = 0
-        directions = [(-1, 0), (-1, 1), (0, 1), (1, 1),
-                        (1, 0), (1, -1), (0, -1), (-1, -1)]
-        for delta_i, delta_j in directions:
-            total_at_count += check_direction(i, j, delta_i, delta_j)
-        if total_at_count < 4:
-            good_positions.append((i, j))
+    m, n = paper_map.shape
+    good_positions = []
 
-# print("Good Positions:", good_positions)
-# # Change map drawing
-# for i, j in good_positions:
-#     paper_map[i, j] = "X"  # Mark good positions on the map
+    for i in range(m):
+        for j in range(n):
+            if paper_map[i, j] == ".":
+                continue
+            # Check each direction. Check how far I can move (not outside map)
+            # If there are fewer than four "@" in all directions combined, save pos (good_positions)
+            total_at_count = 0
+            directions = [(-1, 0), (-1, 1), (0, 1), (1, 1),
+                            (1, 0), (1, -1), (0, -1), (-1, -1)]
+            for delta_i, delta_j in directions:
+                total_at_count += check_direction(i, j, delta_i, delta_j, m, n)
+            if total_at_count < 4:
+                good_positions.append((i, j))
+
+    return good_positions
+
+
 # print(paper_map)
-print("Rolls accesible:", len(good_positions))
+sum_of_good_positions = 0
+n = 1
+while n > 0:
+    good_positions = find_good_positions(paper_map)
+    # print("Good positions found:", len(good_positions))
+    # Remove good positions from map
+    for i, j in good_positions:
+        paper_map[i, j] = "." 
+    # print(paper_map)
+    n = len(good_positions)
+    sum_of_good_positions += n
+
+print("Total sum of good positions: ", sum_of_good_positions)
